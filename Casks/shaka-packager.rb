e2b34b2fad0c6c4a58b8f9885f167e5cd9f6b651
garnajee <>
2024-11-14 15:34:16 +0100

    Initial Commit

---
+++ Casks/shaka-packager.rb
@@ -0,0 +1,17 @@
+cask "shaka-packager" do
+  version "3.4.0"
+  sha256 "97acc637f83a3967e576612c2a627346f3a271bc018949c8e28b076b9cc517fc" 
+
+  url "https://github.com/shaka-project/shaka-packager/releases/download/v#{version}/packager-osx-arm64"
+  name "Shaka Packager"
+  desc "Packager for DASH and HLS streaming"
+  homepage "https://github.com/shaka-project/shaka-packager"
+
+  binary "packager-osx-arm64", target: "packager"
+
+  caveats <<~EOS
+    To use `packager`, run the following command in your terminal:
+      packager --help
+  EOS
+end
+
