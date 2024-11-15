0e304dd1e6a1fc541c3963f5b33b35b7c1d9d75a
garnajee <>
2024-11-15 11:45:44 +0100

    fix: style

--- Casks/shaka-packager.rb
+++ Casks/shaka-packager.rb
@@ -1,6 +1,6 @@
 cask "shaka-packager" do
   version "3.4.0"
-  sha256 "97acc637f83a3967e576612c2a627346f3a271bc018949c8e28b076b9cc517fc" 
+  sha256 "97acc637f83a3967e576612c2a627346f3a271bc018949c8e28b076b9cc517fc"
 
   url "https://github.com/shaka-project/shaka-packager/releases/download/v#{version}/packager-osx-arm64"
   name "Shaka Packager"
@@ -10,7 +10,7 @@ cask "shaka-packager" do
   livecheck do
     url "https://github.com/shaka-project/shaka-packager/releases"
     strategy :page_match
-    regex(/href=.*?\/v?(\d+(?:\.\d+)+)\/packager-osx-arm64/i)
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/packager-osx-arm64}i)
   end
 
   binary "packager-osx-arm64", target: "packager"
@@ -20,4 +20,3 @@ cask "shaka-packager" do
       packager --help
   EOS
 end
-
