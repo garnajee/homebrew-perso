c4e321fc960e1273377bac64cb94dbf8e6d3d0d6
garnajee <>
2024-12-03 14:07:48 +0100

    fix path

--- Casks/nm3u8dl-re.rb
+++ Casks/nm3u8dl-re.rb
@@ -1,12 +1,14 @@
 cask "nm3u8dl-re" do
-  version "0.2.1-beta"
-  sha256 "ed314d1169e23f30faa1fab1bee28fb319a795219cbc7bab47367f406b85d6b3"
+  version "0.3.0-beta"
+  sha256 "eb3488e5af13b969bd3da777f4ff11e514788a966d4bf593df009a8923bdd1fd"
 
-  url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v0.2.1-beta/N_m3u8DL-RE_Beta_osx-arm64_20240828.tar.gz"
+  url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v0.3.0-beta/N_m3u8DL-RE_v0.3.0-beta_osx-arm64_20241203.tar.gz"
   name "N_m3u8DL-RE"
+  desc "Cross-Platform, modern and powerful stream downloader for MPD/M3U8/ISM"
   homepage "https://github.com/nilaoda/N_m3u8DL-RE"
 
-  artifact "N_m3u8DL-RE_Beta_osx-arm64/N_m3u8DL-RE", target: "/usr/local/bin/N_m3u8DL-RE"
+  #artifact "N_m3u8DL-RE_Beta_osx-arm64/N_m3u8DL-RE", target: "/usr/local/bin/N_m3u8DL-RE"
+  binary "#{staged_path}/N_m3u8DL-RE", target: "N_m3u8DL-RE"
 
   # No zap stanza required
 end
