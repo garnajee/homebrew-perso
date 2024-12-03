ab93b0840698c4acea844ec361af9ab139c8fc9a
garnajee <>
2024-12-03 13:56:54 +0100

    update package

--- Formula/nm3u8dl-re.rb
+++ Formula/nm3u8dl-re.rb
@@ -1,8 +1,8 @@
 class Nm3u8dlRe < Formula
   desc "Cross-Platform, modern and powerful stream downloader for MPD/M3U8/ISM."
   homepage "https://github.com/nilaoda/N_m3u8DL-RE"
-  url "https://github.com/nilaoda/N_m3u8DL-RE/archive/refs/tags/v0.2.1-beta.tar.gz"
-  sha256 "ed314d1169e23f30faa1fab1bee28fb319a795219cbc7bab47367f406b85d6b3"
+  url "https://github.com/nilaoda/N_m3u8DL-RE/archive/refs/tags/v0.3.0-beta.tar.gz"
+  sha256 "6cbad6b6fda2733099cabb3cc1f89884984bdfb644416f14713d9bcdd0e55676"
 
   depends_on "dotnet"
 
