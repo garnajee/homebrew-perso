285f452462480a36044b34c60562dd485082dcd7
garnajee <>
2024-12-04 13:39:21 +0100

    fix url/version update for casks

--- Casks/nm3u8dl-re.rb
+++ Casks/nm3u8dl-re.rb
@@ -1,8 +1,10 @@
 cask "nm3u8dl-re" do
   version "0.3.0-beta"
-  sha256 "eb3488e5af13b969bd3da777f4ff11e514788a966d4bf593df009a8923bdd1fd"
+  sha256 arm:   "eb3488e5af13b969bd3da777f4ff11e514788a966d4bf593df009a8923bdd1fd",
+        intel:  "74f9236605042d364e55967e1679dcd6b7a9b3fdf6c66a40175090d27a3c24c6"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v0.3.0-beta/N_m3u8DL-RE_v0.3.0-beta_osx-arm64_20241203.tar.gz"
+  url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v#{version}/N_m3u8DL-RE_v#{version}_osx-#{arch}_20241203.tar.gz"
   name "N_m3u8DL-RE"
   desc "Cross-Platform, modern and powerful stream downloader for MPD/M3U8/ISM"
   homepage "https://github.com/nilaoda/N_m3u8DL-RE"
