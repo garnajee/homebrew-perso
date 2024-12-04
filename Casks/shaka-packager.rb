d2dfcf197a435e424ac5f7b52adde6932c3288ac
garnajee <>
2024-12-04 11:20:52 +0100

    feat/fix: arm+intel for shaka-packager

--- Casks/shaka-packager.rb
+++ Casks/shaka-packager.rb
@@ -1,6 +1,5 @@
 cask "shaka-packager" do
   version "3.4.0"
-  #sha256 "97acc637f83a3967e576612c2a627346f3a271bc018949c8e28b076b9cc517fc"
   sha256 arm:   "97acc637f83a3967e576612c2a627346f3a271bc018949c8e28b076b9cc517fc",
          intel: "74f9236605042d364e55967e1679dcd6b7a9b3fdf6c66a40175090d27a3c24c6"
   arch arm: "arm64", intel: "x64"
@@ -13,7 +12,7 @@ cask "shaka-packager" do
   livecheck do
     url :url
     strategy :page_match
-    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/packager-osx-arm64}i)
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/packager-osx-(?:arm64|x64)}i)
   end
 
   binary "packager-osx-#{arch}", target: "packager"
