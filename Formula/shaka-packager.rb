ed993c6f21846e9730fd8cf58b159fa1e8510229
garnajee <>
2024-12-05 17:38:41 +0100

    fix: desc ; v3.4.0 working

--- Formula/shaka-packager.rb
+++ Formula/shaka-packager.rb
@@ -1,26 +1,34 @@
 class ShakaPackager < Formula
-  desc "SDK for media packaging of DASH/HLS content"
+  desc "SDK for media packaging of DASH/HLS content (includes packager, pssh-box.py, and mpd_generator)"
   homepage "https://github.com/shaka-project/shaka-packager"
   url "https://github.com/shaka-project/shaka-packager.git",
-      using:    :git,
       tag:      "v3.4.0",
-      revision: "1f0a4d1959a17ea77b7f18b608338329395b6a45"
+      revision: "ef1d8995c6c1e6f4df5b9c1d8f7cbb3a8d383bbe",
+      shallow:  true
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "git" => :build
+  depends_on "python@3.11" => :build
 
   def install
     system "git", "submodule", "update", "--init", "--recursive"
-    args = std_cmake_args + ["-G", "Ninja", "-DCMAKE_BUILD_TYPE=Release"]
-    system "cmake", "-B", "build", *args
-    system "cmake", "--build", "build", "--parallel"
-    system "cmake", "--install", "build", "--strip", "--config", "Release"
+
+    mkdir "build" do
+      args = std_cmake_args + [
+        "-G", "Ninja",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DBUILD_SHARED_LIBS=OFF"
+      ]
+      system "cmake", "..", *args
+      system "cmake", "--build", ".", "--parallel"
+      system "cmake", "--install", ".", "--strip"
+    end
   end
 
   test do
-    system "#{bin}/packager", "-h"
+    assert_match "packager", shell_output("#{bin}/packager -h")
   end
 end
 
