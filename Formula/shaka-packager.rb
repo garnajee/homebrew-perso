a55442c2d3e44d351b1213de5091a1b82588a449
garnajee <>
2024-12-05 14:01:28 +0100

    new shaka-packager formula - test

--- Formula/shaka-packager.rb
+++ Formula/shaka-packager.rb
@@ -1,24 +1,26 @@
 class ShakaPackager < Formula
-  desc "Packager for DASH and HLS streaming"
+  desc "SDK for media packaging of DASH/HLS content"
   homepage "https://github.com/shaka-project/shaka-packager"
-  url "https://github.com/shaka-project/shaka-packager/archive/refs/tags/v3.4.0.tar.gz"
-  sha256 "93d9614ce181ef42e596c65d75fec6c9152b7f3308d1cafcaed006e79a1aeb1a"
-  license "BSD 3 clause"
+  url "https://github.com/shaka-project/shaka-packager.git",
+      using:    :git,
+      tag:      "v3.4.0",
+      revision: "1f0a4d1959a17ea77b7f18b608338329395b6a45"
+  license "BSD-3-Clause"
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
+  depends_on "git" => :build
 
   def install
     system "git", "submodule", "update", "--init", "--recursive"
-    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
-                    "-DCMAKE_BUILD_TYPE=Release",
-                    "-DBUILD_TESTING=OFF",
-                    *std_cmake_args
+    args = std_cmake_args + ["-G", "Ninja", "-DCMAKE_BUILD_TYPE=Release"]
+    system "cmake", "-B", "build", *args
     system "cmake", "--build", "build", "--parallel"
-    system "cmake", "--install", "build", "--strip"
+    system "cmake", "--install", "build", "--strip", "--config", "Release"
   end
 
   test do
-    assert_match "Shaka Packager version", shell_output("#{bin}/packager --version")
+    system "#{bin}/packager", "-h"
   end
 end
+
