568c81fb512bf06bbbd7b2a6655f65b0cd297d83
garnajee <62147746+garnajee@users.noreply.github.com>
2025-12-12 22:31:41 +0100

    update formula to take binary (build impossible) and update ci/cd

--- Formula/nm3u8dl-re.rb
+++ Formula/nm3u8dl-re.rb
@@ -1,25 +1,26 @@
 class Nm3u8dlRe < Formula
-  desc "Cross-Platform, modern and powerful stream downloader for MPD/M3U8/ISM."
+  desc "Cross-Platform, modern and powerful stream downloader for MPD/M3U8/ISM"
   homepage "https://github.com/nilaoda/N_m3u8DL-RE"
-  url "https://github.com/nilaoda/N_m3u8DL-RE/archive/refs/tags/v0.3.0-beta.tar.gz"
-  sha256 "6cbad6b6fda2733099cabb3cc1f89884984bdfb644416f14713d9bcdd0e55676"
+  url "https://github.com/nilaoda/N_m3u8DL-RE/archive/refs/tags/v0.5.1-beta.tar.gz"
+  sha256 "a4532328b9d31d4511d73a7c6f05929824c6575972856f610f44358892f3de6c"
   license "MIT"
 
   depends_on "dotnet" => :build
 
   def install
-    if Hardware::CPU.arm?
-      rid = "osx-arm64"
-    else
-      rid = "osx-x64"
-    end
+    arch = Hardware::CPU.intel? ? "x64" : "arm64"
+    rid = "osx-#{arch}"
 
-    system "dotnet", "publish", "src/N_m3u8DL-RE", "-r", rid, "-c", "Release", "-o", "artifact"
+    system "dotnet", "publish", "src/N_m3u8DL-RE", 
+           "--configuration", "Release", 
+           "--runtime", rid, 
+           "--self-contained", "false",
+           "--output", "artifact"
 
     bin.install "artifact/N_m3u8DL-RE" => "n_m3u8dl-re"
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/n_m3u8dl-re --help")
+    assert_match version.to_s, shell_output("#{bin}/n_m3u8dl-re --version")
   end
 end
--- Formula/shaka-packager.rb
+++ Formula/shaka-packager.rb
@@ -1,33 +1,27 @@
 class ShakaPackager < Formula
   desc "SDK for media packaging of DASH/HLS content (includes packager, pssh-box.py, and mpd_generator)"
   homepage "https://github.com/shaka-project/shaka-packager"
-  url "https://github.com/shaka-project/shaka-packager.git",
-      tag:      "v3.4.0",
-      revision: "1f0a4d1959a17ea77b7f18b608338329395b6a45",
-      shallow:  true
+  version "3.4.2"
   license "BSD-3-Clause"
 
-  depends_on "cmake" => :build
-  depends_on "ninja" => :build
-  depends_on "git" => :build
-  depends_on "python@3.11" => :build
+  if Hardware::CPU.arm?
+    url "https://github.com/shaka-project/shaka-packager/releases/download/v#{version}/packager-osx-arm64"
+    sha256 "d97cc27e2b003dd56937c54eb08f0c3f61637ad976a2d98b8bf2adaf041cb1c2"
+  else
+    url "https://github.com/shaka-project/shaka-packager/releases/download/v#{version}/packager-osx-x64"
+    sha256 "cd70359fe9dadca88cce4a7ec32d93184d13f3439cf0f43fd95156dd8b8135df"
+  end
 
   def install
-    system "git", "submodule", "update", "--init", "--recursive"
-
-    mkdir "build" do
-      args = std_cmake_args + [
-        "-G", "Ninja",
-        "-DCMAKE_BUILD_TYPE=Release",
-        "-DBUILD_SHARED_LIBS=OFF"
-      ]
-      system "cmake", "..", *args
-      system "cmake", "--build", ".", "--parallel"
-      system "cmake", "--install", ".", "--strip"
+    if Hardware::CPU.arm?
+      bin.install "packager-osx-arm64" => "packager"
+    else
+      bin.install "packager-osx-x64" => "packager"
     end
+    chmod 0755, bin/"packager"
   end
 
   test do
-    assert_match "packager", shell_output("#{bin}/packager -h")
+    assert_match "packager version", shell_output("#{bin}/packager --version")
   end
 end
