19fcb14d3e8839043f5b57491142ef3bedc8be10
garnajee <62147746+garnajee@users.noreply.github.com>
2025-12-12 22:46:28 +0100

    fix: comma

--- Casks/shaka-packager.rb
+++ Casks/shaka-packager.rb
@@ -1,6 +1,6 @@
 cask "shaka-packager" do
   version ""
-  sha256 arm:   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",,
+  sha256 arm:   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
         intel:  "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   arch arm: "arm64", intel: "x64"
 
